aefd94ef6b544758eda1c260760fce57e20c7c53
deividsonsabino <deividsonsabino@outlook.com.br>
2023-08-04 13:21:03 -0300

    feat: add config tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+      <div className="bg-[url('/bg-soccer.jpg')] bg-cover">
+        <div className="w-full h-screen bg-blue-950 opacity-75">
+        sasasa
+        </div>
+      </div>
+    )
+  }
+  
